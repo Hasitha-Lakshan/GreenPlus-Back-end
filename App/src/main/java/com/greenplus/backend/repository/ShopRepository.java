15467a309e035b94fbd7117b0cb8d8e8780def32
Hasitha <ranaweerahlakshan@gmail.com>
2020-11-27 06:29:44 +0530

    Implemented fetach shop details data by using farmers' username

--- App/src/main/java/com/greenplus/backend/repository/ShopRepository.java
+++ App/src/main/java/com/greenplus/backend/repository/ShopRepository.java
@@ -1,11 +1,16 @@
 package com.greenplus.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.greenplus.backend.model.Shop;
+import com.greenplus.backend.model.User;
 
 @Repository
 public interface ShopRepository extends JpaRepository<Shop, Integer> {
+
+	List<Shop> findByUser(User user);
 
 }
--- App/src/main/java/com/greenplus/backend/service/FarmerService.java
+++ App/src/main/java/com/greenplus/backend/service/FarmerService.java
@@ -1,10 +1,14 @@
 package com.greenplus.backend.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.greenplus.backend.dto.Response;
 import com.greenplus.backend.dto.ShopCreatingRequest;
+import com.greenplus.backend.dto.ShopDetailsResponse;
 import com.greenplus.backend.model.Shop;
 import com.greenplus.backend.model.User;
 import com.greenplus.backend.repository.ShopRepository;
@@ -68,4 +72,38 @@ public class FarmerService {
 
 	}
 
+	private ShopDetailsResponse mapFromShopToDto(Shop shop) {
+
+		ShopDetailsResponse shopDetailsResponse = new ShopDetailsResponse();
+
+		shopDetailsResponse.setShopId(shop.getShopId());
+		shopDetailsResponse.setTitle(shop.getTitle());
+		shopDetailsResponse.setCategory(shop.getCategory());
+		shopDetailsResponse.setSubCategory(shop.getSubCategory());
+		shopDetailsResponse.setDescription(shop.getDescription());
+		shopDetailsResponse.setQuantity(shop.getQuantity());
+		shopDetailsResponse.setPrice(shop.getPrice());
+		shopDetailsResponse.setLocation(shop.getLocation());
+		shopDetailsResponse.setCreatedDate(shop.getCreatedDate());
+		shopDetailsResponse.setCreatedTime(shop.getCreatedTime());
+		shopDetailsResponse.setDeliveryTime(shop.getDeliveryTime());
+		shopDetailsResponse.setShopStatus(shop.isShopStatus());
+
+		return shopDetailsResponse;
+	}
+
+	public List<ShopDetailsResponse> getShopsByUser(String username) {
+
+		User user = userRepository.findByUsername(username);
+
+		if (user != null && user.getRole().equals("FARMER")) {
+
+			List<Shop> shops = shopRepository.findByUser(user);
+
+			return shops.stream().map(this::mapFromShopToDto).collect(Collectors.toList());
+		} else {
+			return null;
+		}
+
+	}
 }
